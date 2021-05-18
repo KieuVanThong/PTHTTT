ccd5e9944ca945935c32d96360c99c59445edc04
KieuVanThong <74952809+KieuVanThong@users.noreply.github.com>
2021-05-18 12:32:11 +0700

    Update Kho.java

--- src/main/java/pthttt/entity/Kho.java
+++ src/main/java/pthttt/entity/Kho.java
@@ -18,6 +18,7 @@ public class Kho implements Serializable{
 
 	@Id
 	@Column(name = "ID",nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int ID;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
