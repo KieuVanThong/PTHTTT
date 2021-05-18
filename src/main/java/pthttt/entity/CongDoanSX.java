8bef10b83fb013dec21e9d547e2605a0f291b046
KieuVanThong <74952809+KieuVanThong@users.noreply.github.com>
2021-05-18 12:31:50 +0700

    Update CongDoanSX.java

--- src/main/java/pthttt/entity/CongDoanSX.java
+++ src/main/java/pthttt/entity/CongDoanSX.java
@@ -18,6 +18,7 @@ public class CongDoanSX implements Serializable{
 
 	@Id
 	@Column(name = "ID",nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int ID;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
